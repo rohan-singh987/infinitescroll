6a61b017c0314bcccd4e9a9d21b9656e81d03818
Rohan Singh <rohannsingh98765@gmail.com>
2023-09-22 02:49:03 +0530

    completing project

---
+++ src/Context/DataState.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import dataContext from './DataContext'
+import axios from 'axios'
+
+const DataState = (props) => {
+
+    const [fetchedData, setFetchedData] = useState([]);
+
+    const getData = async () => {
+        try {
+            const result = await axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=100&_page=${1}`)
+            // const result = await axios.get(`https://api.example.com/items?page=:${page}&limit=:9`)
+            setFetchedData(result.data)
+        }
+        catch (error) {
+            console.error("An error occured: ", error);
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+  return (
+    <dataContext.Provider value={fetchedData}>
+        {props.children}
+    </dataContext.Provider>
+  )
+}
+
+export default DataState
